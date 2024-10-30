5aea00c0f8b19f34fef954aa9420cf25443f6a81
Toshiro19-w <6451071036@st.utc2.edu.vn>
2024-10-30 15:21:01 +0700

    Add files via upload

--- menu/script.js
+++ menu/script.js
@@ -1,8 +1,6 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
@@ -12,6 +10,16 @@ loginLink.addEventListener('click', ()=>{
     wrapper.classList.remove('active');
 });
 
-btnPopup.addEventListener('click', ()=>{
-    wrapper.classList.add('active-popup');
-});
+function saveAccountInfo() {
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Lưu thông tin vào Local Storage
+    localStorage.setItem('username', username);
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+
+    // Điều hướng đến trang account.html
+    window.location.href = '/html/menu.html';
+}
